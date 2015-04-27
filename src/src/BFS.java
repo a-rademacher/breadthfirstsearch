dc47e60438bf10a4fe90680940c85dbe00f14142
Rainmaker <rademacheraustin@gmail.com>
2015-04-26 22:08:01 -0400

    Initial Commit

---
+++ src/src/BFS.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+
+/**
+ *
+ * @author Rainmaker
+ */
+public class BFS {
+    
+    private boolean[][] adjacenyMatrix;
+    
+    /**
+     * 
+     * @param matrixX - The X size of Matrix.
+     * @param matrixY - The Y size of Matrix.
+     */
+    public void setAdjacenyMatrix(int matrixX, int matrixY){
+        adjacenyMatrix = new boolean[matrixX][matrixY];
+    }
+    
+    
+    public void setValue(String input){
+        
+    }
+    
+    private void setMatrixValues(int[] input){
+        
+    }
+    
+}
